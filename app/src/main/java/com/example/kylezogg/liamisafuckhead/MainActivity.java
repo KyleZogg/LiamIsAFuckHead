71d012605a5a97cf3733422ed30b4570febc0da7
Kyle Zogg <kgzskier2@gmail.com>
2016-04-17 20:03:44 -0500

    Bitch

--- app/src/main/java/com/example/kylezogg/liamisafuckhead/MainActivity.java
+++ app/src/main/java/com/example/kylezogg/liamisafuckhead/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.kylezogg.liamisafuckhead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//Liam is still a fuck heaed
 public class MainActivity extends AppCompatActivity {
 
     @Override
